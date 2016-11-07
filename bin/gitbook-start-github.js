d2e6bb3da6484bfe59fa4002775cd875a38d01b9
alu0100782851 <alu0100782851@ull.edu.es>
2016-11-07 21:34:05 +0000

    Cambiando ejecutable principal

--- bin/gitbook-start-github.js
+++ bin/gitbook-start-github.js
@@ -4,17 +4,18 @@ var modulo_repo = require("./create_repo")
 var fs = require('fs');
 var path = require('path');
 var child = require("child_process");
+var simplegit = require("simple-git")(path.join(process.cwd())); //revisar esta ruta luego por si da problemas
 
     /*
     Funcionamiento:
-        1. primero crea el repo
+        1. crea el repo
         2. añade la tarea al gulpfile
         3. copia gulpfile a tu directorio
         4. añade el remoto de github para que puedas subir cambios
     */
 
 
-modulo_repo.pedirdatos();
+//modulo_repo.pedirdatos();
 
 
 function initialize(directorio) {
@@ -25,8 +26,6 @@ function initialize(directorio) {
     
     var contenido='\ngulp.task("deploy-github", function () {'+ 
         '\n\tvar github = require("gitbook-start-github-alex-moi");'+
-        '\n\tvar url = paquete.repository.url;'+
-        
         '\n\n\ github.deploy();'+
         '\n});\n\n';
 
@@ -36,10 +35,9 @@ function initialize(directorio) {
     
     //añadimos la tarea
     fs.writeFileSync(path.join(process.cwd(), 'node_modules','gitbook-start-alex-moi-nitesh','gulpfile.js'), contenido,  {'flag':'a'},  function(err) {
-        if (err) {
+        if (err) 
             return console.error(err);
-        }
-        console.log("Añadiendo tarea gulp")
+            console.log("Añadiendo tarea gulp")
     });
     
     
@@ -47,7 +45,7 @@ function initialize(directorio) {
     fs.copyFile(path.join(process.cwd(), 'node_modules','gitbook-start-alex-moi-nitesh','gulpfile.js'), path.join(process.cwd(), directorio , 'gulpfile.js'),function(err){
         if(err)
           console.log(err);
-         console.log("Tarea gulp GITHUB añadida a gulpfile")
+          console.log("Tarea gulp GITHUB añadida a gulpfile")
     });
 
 
@@ -57,14 +55,7 @@ function deploy() {
 
     console.log("Comenzando el deploy en GITHUB");
    
-    
-    child.exec('git add . ; git commit -m "First commit"; git push origin master;', function(error, stdout, stderr){
-        if(error)
-          console.log(error)
-        
-        console.log(stderr);
-        console.log(stdout);
-    })
+    simplegit.add('.').commit('Update repo').push(['-uf', 'origin', 'master'], function (err, data) {});
 
 };
 
