57cf1b89133ec930ba93937371685af1811eaf2e
alu0100767421 <alu0100767421@ull.edu.es>
2016-11-10 19:49:45 +0000

    quitando restriccion del paquete del iaas

--- bin/gitbook-start-alex-moi-nitesh.js
+++ bin/gitbook-start-alex-moi-nitesh.js
@@ -119,6 +119,18 @@ function crear_estructura(dir){
 function desplegar(nombre_dir, paquete){
       /*crear_estructura(nombre_dir);*/
       
+      if(ip_iaas){
+        var paque = require(path.resolve(process.cwd(),"package.json"));
+        paque.iaas.IP=ip_iaas;
+        console.log("Ip "+paque.iaas.IP)
+        console.log("Ip "+paque.main)
+      }
+      if(path_iaas){
+        var paque = require(path.resolve(process.cwd(),"package.json"));
+        paque.iaas.PATH=path_iaas;
+      }
+      
+      
       child.exec('npm install -g gitbook-start-'+paquete+'-alex-moi', function(error, stdout, stderr){
         if(error)
           console.log(error)
@@ -207,11 +219,12 @@ else{
                 
                 switch (deploy) {
                   case 'iaas-ull-es':
-                      if(ip_iaas && path_iaas){ 
+                      /*if(ip_iaas && path_iaas){ 
                           desplegar(nombre_dir, 'iaas-ull-es')               
                       }
                       else
-                            console.log("Especifique la ip y el path del iaas")
+                            console.log("Especifique la ip y el path del iaas")*/
+                        desplegar(nombre_dir, 'iaas-ull-es')  ;   
                       break;
                       
                   case 'heroku':
