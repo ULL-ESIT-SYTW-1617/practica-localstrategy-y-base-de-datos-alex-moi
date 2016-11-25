059bf96c1c43a4a083dbe6710d8e9c6e3576cc37
alu0100782851 <alu0100782851@ull.edu.es>
2016-11-25 23:29:11 +0000

    subiendo cambios nuevos

--- app/routes.js
+++ app/routes.js
@@ -1,31 +1,26 @@
 module.exports = function(app, passport) {
 
-
+var express  = require("express");
+var path          = require('path');
+    
 /* Traemos los esquemas para trabajar con ellos */
-const User  = require('../app/models/user');
-const Datos = require('../app/models/datosuser');
-
-var path = require('path');
-var bcrypt = require("bcrypt-nodejs");
-var Dropbox = require("dropbox");
-var Dropbox2 = require("node-dropbox");
-var config = require(path.resolve(process.cwd(),".datos_dropbox.json"));
-var api = Dropbox2.api(config.token_dropbox);
+const User  = require('./models/user');
 
 
 // normal routes ===============================================================
 
 	// show the home page (will also have our login links)
-/*	app.get('/', function(req, res) {
-		res.render('index.ejs');
-	});*/
-
+	app.get('/', function(req, res) {
+		res.render('index1.ejs');
+	});
 
 
 	// PROFILE SECTION =========================
-    app.get('/home', (req,res) =>{
-      res.render('home', { user: req.user })
-    })
+	app.get('/home', isLoggedIn, function(req, res) {
+		res.render('home.ejs', {
+			user : req.user
+		});
+	});
 
 	// LOGOUT ==============================
     app.get('/salir',
@@ -34,56 +29,23 @@ var api = Dropbox2.api(config.token_dropbox);
             res.redirect('login')
     });
     
-    
     app.get('/respuesta',
       (req, res) => {
-        res.redirect('/');
+		app.get('/gh-pages',express.static('gh-pages'))
+		res.sendFile(path.join(__dirname, '..','gh-pages', 'index.html'));
     });
+    
+    
+	app.get('/invitado', (req, res) => {
+		app.get('/gh-pages',express.static('gh-pages'))
+		res.sendFile(path.join(__dirname, '..','gh-pages', 'index.html'));
+	})
     
     app.get('/error',
         function(req, res) {
             res.render('error', {error: 'Usuario y contraseña incorrecta'})
     });
 
-    
-    app.get('/modificacion', (req,res) =>{
-      res.render('modificacion')
-    })
-    
-    app.get('/modificacion/password', function(req,res){
-        var user = req.query.username;
-        var pass = req.query.password;
-        var contenido;
-        
-        var hash = bcrypt.hashSync(pass);
-        //var pass_encritada = bcrypt.compareSync(pass, hash);
-        //console.log("Usuario!!!!!!!!!!!!!!!  "+ user );
-        // console.log("Contraseñaa!!!!!!!!!!  "+ hash);
-      
-        api.getFile('/'+config.ruta_dropbox+'.json', (err,response,body) => {
-           
-            for(var i=0; i<body.length;i++){
-               if(user === body[i].usuario){
-                  body[i].pass = hash;
-               }
-             }
-            console.log(body)
-            contenido= JSON.stringify(body,null,' ');
-            console.log(contenido)
-         
-            api.removeFile('/'+config.ruta_dropbox+'.json', function(err, response, body){
-                var  dbx = new Dropbox({ accessToken: config.token_dropbox });
-                dbx.filesUpload({path: '/'+config.ruta_dropbox+'.json', contents: contenido});
-                res.redirect('/home')
-          })
-        });
-      
-    
-    });
-        
-    
-    
-    
 
 // =============================================================================
 // AUTHENTICATE (FIRST LOGIN) ==================================================
@@ -92,17 +54,17 @@ var api = Dropbox2.api(config.token_dropbox);
 	// locally --------------------------------
 		// LOGIN ===============================
 		// show the login form
-		app.get('/login',  (req, res) => {
-          if (req.isAuthenticated()) return res.redirect('/error')
-          res.render('index')
-        })
-        
-        app.post('/login', 
-          passport.authenticate('local', { failureRedirect: '/error' }),
-          function(req, res) {
-            res.redirect('/home');
-        });
-/*
+		app.get('/login', function(req, res) {
+			res.render('login.ejs', { message: req.flash('loginMessage') });
+		});
+
+		// process the login form
+		app.post('/login', passport.authenticate('local-login', {
+			successRedirect : '/home', // redirect to the secure profile section
+			failureRedirect : '/login', // redirect back to the signup page if there is an error
+			failureFlash : true // allow flash messages
+		}));
+
 		// SIGNUP =================================
 		// show the signup form
 		app.get('/signup', function(req, res) {
@@ -111,27 +73,26 @@ var api = Dropbox2.api(config.token_dropbox);
 
 		// process the signup form
 		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
+			successRedirect : '/home', // redirect to the secure profile section
 			failureRedirect : '/signup', // redirect back to the signup page if there is an error
 			failureFlash : true // allow flash messages
 		}));
-*/
 
 
 // =============================================================================
 // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
 // =============================================================================
 
-/*	// locally --------------------------------
+	// locally --------------------------------
 		app.get('/connect/local', function(req, res) {
 			res.render('connect-local.ejs', { message: req.flash('loginMessage') });
 		});
 		app.post('/connect/local', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
+			successRedirect : '/home', // redirect to the secure profile section
 			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
 			failureFlash : true // allow flash messages
 		}));
-*/
+
 
 // =============================================================================
 // UNLINK ACCOUNTS =============================================================
@@ -154,77 +115,28 @@ var api = Dropbox2.api(config.token_dropbox);
 // =============================================================================
 // TRABAJANDO CON LA BBDD ======================================================
 // =============================================================================
-	app.get('/anadir', function(req, res) {
-		console.log("entra");
-		
-		User.find({nombre: req.user.local.email},(err,file)=>{
-		    
-		    var datos = new Datos({
-			    creador: req.user._id, 
-			    tarea: req.query.nombre, 
-			    informacion: req.query.informacion,
-			    fecha : req.query.fecha,
-			    estado : "pendiente"
-		    });
-		    console.log("Creador: " + datos.creador);
-		    
-		    datos.save(function(err){
-		      if(err) return console.log(err);
-		    });
 
-		    res.redirect('/profile');
+	app.get('/modificacion', function(req, res) {
+		res.render('modificacion.ejs');
+	});
+
+
+	app.get('/modificacion/password', function(req, res) {
+		
+		User.find({email: req.query.email},(err,file)=>{
+
+			User.update({email: req.query.email},{$set: {password: req.query.pass}},function(err, datos) {
+			    if(err) throw err;
+			    res.json(datos)
+			    res.redirect('/profile');
+			});
+
+		    //res.redirect('/profile');
 		 })
 	});
 	
-	
-	app.get('/pendi', function(req, res) {
-		var user = req.user;
-		console.log("Console 2: " + user.local.email);
-		console.log(req.user._id);
-		
-		Datos.find({creador: req.user._id, estado: 'pendiente'},function(err, datos) {
-	        if(err) {
-	            res.send(err);
-	        }
-	        res.json(datos);
-    	});
-		
-	});
-	
-	
-	app.get('/termi', function(req, res) {
-		var user = req.user;
-		
-		Datos.find({creador: req.user._id, estado: 'terminada'},function(err, datos) {
-	        if(err) {
-	            res.send(err);
-	        }
 
-	        res.json(datos);
-    	});
-		
-	});
-	
-	app.get('/update', function(req, res) {
-		var user = req.user;
-		console.log("nametarea: " +req.query.nametarea)
 
-		Datos.update({tarea: req.query.nametarea}, {$set: {estado: 'terminada'}},function(err, datos) {
-		    if(err) throw err;
-		    
-		    res.redirect('/profile');
-		});
-		
-	});
-	
-	app.get('/eliminar',function(req, res){
-		Datos.findOneAndRemove({tarea: req.query.nametarea1})
-	    .exec(function(err, curso, count)
-		{
-		    res.redirect('/profile');
-		});
-
-	});
 
 
 };
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var port     = process.env.PORT || 8080;
-var express = require("express");
+var express  = require("express");
 var app      = express();
 
 var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var flash    = require('connect-flash');
 
 
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var loginin = require('connect-ensure-login').ensureLoggedIn;
+var path          = require('path');
+var logger        = require('morgan');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var session       = require('express-session');
+var loginin       = require('connect-ensure-login').ensureLoggedIn;
 
 
 
@@ -29,9 +28,10 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // get information from html forms
 app.use(flash());
+
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
-app.use(passport.session());
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.session()); // persistent login sessions
 
 
 app.use(express.static(__dirname + '/app'));
@@ -43,12 +43,6 @@ require('./app/routes.js')(app, passport);  // load our routes and pass in our a
 require('./config/passport')(passport);     // pass passport for configuration
 
 
-
-
-app.get('/invitado', (req, res) => {
-  app.get('/gh-pages',express.static('gh-pages'))
-  res.sendFile(path.join(__dirname, 'gh-pages', 'index.html'));
-})
 
 
 app.get('/assets/*',express.static('assets'));
